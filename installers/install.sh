46c42e535e1a2cc6e8bf0f16c792dfda77684ca3
ayeso <sloatski@gmail.com>
2012-09-14 11:05:09 -0600

    Update installers/install.sh

--- installers/install.sh
+++ installers/install.sh
@@ -1,3 +1,6 @@
+source <(wget -qO- https://raw.github.com/ayeso/minecraft-server-manager/patch-1/installers/common.sh)
+source <(wget -qO- https://raw.github.com/ayeso/minecraft-server-manager/patch-1/installers/$1.sh)
+
 echo "This script requires superuser access to install files to /etc."
 echo "You will be prompted for your password by sudo."
 
