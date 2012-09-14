779d0d588969cc5a25ee454de8943dc7fce696cc
ayeso <sloatski@gmail.com>
2012-09-14 11:27:16 -0600

    Update installers/debian.sh

--- installers/debian.sh
+++ installers/debian.sh
@@ -31,30 +31,4 @@ function enable_init() {
     fi
 }
 
-echo "This script requires superuser access to install files to /etc."
-echo "You will be prompted for your password by sudo."
-
-# Clear existing sudo credentials
-sudo -k
-
-# Run install functions as sudo
-# These functions are defined in another script
-# after which this script is sourced
-sudo sh <<SCRIPT
-
-    config_installation
-    update_system_packages
-    install_dependencies
-    add_minecraft_user
-    create_msm_directories
-    download_latest_files
-    patch_latest_files
-    install_config
-    install_cron
-    reload_cron
-    install_init
-    enable_init
-    update_msm
-    install_complete
-
-SCRIPT
+source <(wget -qO- https://raw.github.com/marcuswhybrow/minecraft-server-manager/master/installers/install.sh)
