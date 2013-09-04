Silthus/minecraft-server-manager

Whoops, fixed UPDATE_URL to use the master branch.

installers/debian.sh
@@ -1,4 +1,4 @@
-UPDATE_URL="https://raw.github.com/IronTetsubo/minecraft-server-manager/latest"
+UPDATE_URL="https://raw.github.com/IronTetsubo/minecraft-server-manager/master"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/msmcommon.sh
 source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
 

msmhq/msm

Conflicts:
	installers/debian.sh
	installers/redhat.sh

installers/debian.sh
@@ -1,4 +1,4 @@
-UPDATE_URL="https://raw.github.com/marcuswhybrow/minecraft-server-manager/latest"
+UPDATE_URL="https://raw.github.com/marcuswhybrow/minecraft-server-manager/master"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/msmcommon.sh
 source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
 