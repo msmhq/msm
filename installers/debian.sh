0694e5dc746e1d66dbdb92de3c2f3e303daab4ce
Kadah <kadah.coba@gmail.com>
2018-10-04 15:22:56 -0700

    Enable universe repository on Ubuntu for jq package

--- installers/debian.sh
+++ installers/debian.sh
@@ -6,6 +6,12 @@ source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
 
 function update_system_packages() {
     install_log "Updating sources"
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [ $NAME == 'Ubuntu' ]; then
+            sudo add-apt-repository universe || install_error "Couldn't enable universe repository"
+        fi
+    fi
     sudo apt-get update || install_error "Couldn't update package list"
     sudo apt-get upgrade || install_error "Couldn't upgrade packages"
 }
