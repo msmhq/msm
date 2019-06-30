8bfdd3b5249527602a4795739e02eee900f242f7
Don Viszneki <don@codebad.com>
2019-06-30 14:50:50 -0700

    using github hdon master

--- installers/debian.sh
+++ installers/debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UPDATE_URL="https://raw.githubusercontent.com/msmhq/msm/master"
+UPDATE_URL="https://raw.githubusercontent.com/hdon/msm/master"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/msmcommon.sh
 source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
 
