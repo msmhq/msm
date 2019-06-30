61b89c40f00fc6199ba95720b2c5928ee74ce78d
Don Viszneki <don@codebad.com>
2019-06-30 13:05:57 -0700

    bugfix new_user_system setting

--- installers/common.sh
+++ installers/common.sh
@@ -33,7 +33,7 @@ function config_installation() {
 
     echo -n "Add new user as system account? [y/N]: "
     read answer
-    if [[ $answer != "y" ]]; then
+    if [ $answer = "y" ]; then
         msm_user_system=true
     fi
 
